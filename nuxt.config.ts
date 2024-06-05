534e6a820fde189493f9065454df572894726c05
syahendra <syahendra@privyid.tech>
2024-06-05 16:21:33 +0700

    fix: pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,3 @@
-import { VitePWA } from 'vite-plugin-pwa'
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
@@ -25,34 +23,6 @@ export default defineNuxtConfig({
   css: ['~/assets/css/font.css'],
   colorMode: {
     preference: 'light',
-  },
-  vite: {
-    plugins: [
-      VitePWA({
-        registerType: 'autoUpdate',
-        manifest: {
-          name: 'MSAme',
-          short_name: 'App',
-          description: 'MSA me syahendra.com',
-          theme_color: '#ffffff',
-          icons: [
-            {
-              src: 'icons/icon-192x192.png',
-              sizes: '192x192',
-              type: 'image/png',
-            },
-            {
-              src: 'icons/icon-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-            },
-          ],
-        },
-        workbox: {
-          // Workbox configuration options
-        },
-      }),
-    ],
   },
   pwa: {
     mode: 'development',
