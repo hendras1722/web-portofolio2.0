c4c4312424433a4174e79aa545a0bc8267e6d87a
Muh Syahendra Anindyantoro <57746184+hendras1722@users.noreply.github.com>
2025-01-12 20:43:45 +0700

    fix: fixing cv

--- pages/curriculum-vitae.vue
+++ pages/curriculum-vitae.vue
@@ -1,10 +1,20 @@
 <template>
-  <div class="flex flex-col min-h-screen my-2">
+  <div class="flex flex-col min-h-screen my-2" ref="refHtml">
     <div>
-      <div class="rounded-full bg-[url(~/assets/me.jpg)] w-[200px] h-[200px] bg-cover bg-center mb-5">
-        <img src="~/assets/me.jpg" class=" w-[200px] h-[200px] object-cover rounded-full" alt="me" />
+      <div>
+        <div class="rounded-full bg-[url(~/assets/me.jpg)] w-[200px] h-[200px] bg-cover bg-center mb-5">
+          <img src="~/assets/me.jpg" class=" w-[200px] h-[200px] object-cover rounded-full" alt="me" />
+        </div>
+        <h1 class="text-5xl font-bold">Muhammad Syahendra Anindyantoro</h1>
       </div>
       <small>Yogyakarta, 2024</small>
+      <div class="text-[12px] font-semibold">
+        Phone: 089663604258
+      </div>
+      <div class="text-[12px] font-semibold">
+        Address: Perumahan Griya Pratama Asri 2 No.H-12, Somoragen, Joho, Kec. Prambanan, Kabupaten Klaten, Jawa Tengah
+        57454
+      </div>
     </div>
     <div class="mt-5 flex-1">
       <p>
@@ -34,6 +44,14 @@
           <span class="text-nowrap font-bold">Stack technologies</span>
           <div class="w-full">
             <div class="border-2 rounded border-[#3D3C3C] w-full"></div>
+          </div>
+        </div>
+        <div class="flex flex-wrap">
+          <div v-for=" logo in logos" :key="logo">
+            <a :href="'https://github.com/hendras1722'" target="_blank" v-if="logo.src.includes('github')">
+              <img :src="logo.src" :alt="logo.src" />
+            </a>
+            <img :src="logo.src" :alt="logo.src" v-else />
           </div>
         </div>
       </div>
@@ -84,6 +102,40 @@
         </div>
         <div class="grid place-items-center my-5">
           <div class="border-2 rounded border-[#3D3C3C] w-2/3 opacity-25"></div>
+        </div>
+        <div class="flex items-start relative">
+          <div class="absolute right-0 my-auto top-0  mx-auto flex justify-start items-start h-fit -z-10 opacity-50">
+            <img alt="icon" src="~/assets/nashta.jpg" width="200" />
+          </div>
+          <div>
+            <div class="font-bold">Frontend Developer</div>
+            <div class="my-1">PT. NashTa Global Utama · Contract</div>
+            <div class="my-1 text-[12px] text-[#3D3C3C]">
+              Jul 2020 - Jun 2021 · 1 yr
+            </div>
+            <div class="text-[12px] text-[#3D3C3C]">Depok, Jawa Barat, Indonesia</div>
+            <p>
+              On site Bea Cukai CEISA 4.0.
+            <ul class="list-disc ml-5">
+              <li> Develop module by bea cukai </li>
+              <li> Maintenance Application CEISA </li>
+              <li> Develop module Perijinan, and Manifest bea cukai</li>
+            </ul>
+            </p>
+          </div>
+        </div>
+        <div class="grid place-items-center my-5">
+          <div class="border-2 rounded border-[#3D3C3C] w-2/3 opacity-25"></div>
+        </div>
+        <div class="flex items-start relative">
+          <div>
+            <div class="font-bold">Technical Support Engineer</div>
+            <div class="my-1">PT. Binter Jet Indonesia</div>
+            <div class="my-1 text-[12px] text-[#3D3C3C]">
+              Feb 2019 - Mar 2019 · 2 mos
+            </div>
+            <div class="text-[12px] text-[#3D3C3C]">Provinsi Jawa Tengah, Indonesia</div>
+          </div>
         </div>
       </div>
 
@@ -198,14 +250,97 @@
             <div class="font-bold text-[10px]">Nuxt</div>
           </div>
         </div>
+        <div class="border rounded-lg border-[#3D3C3C] p-5 first:mt-0 my-2 last:mb-0">
+          <div>
+            <div class="font-bold">
+              Ronin Dashboard </div>
+          </div>
+          <div class="text-[12px]">
+            Application privy internal for check log by Application privy orther
+          </div>
+          <div class="border border-black p-1 my-2 rounded-lg w-fit">
+            <div class="font-bold text-[10px]">Nuxt</div>
+          </div>
+        </div>
+        <div class="border rounded-lg border-[#3D3C3C] p-5 first:mt-0 my-2 last:mb-0">
+          <div>
+            <div class="font-bold">
+              BNI Regsand </div>
+          </div>
+          <div class="text-[12px]">
+            Maintenance dashboard and landing for applying credit card bank BNI
+          </div>
+          <div class="border border-black p-1 my-2 rounded-lg w-fit">
+            <div class="font-bold text-[10px]">Nuxt</div>
+          </div>
+        </div>
+        <div class="border rounded-lg border-[#3D3C3C] p-5 first:mt-0 my-2 last:mb-0">
+          <div>
+            <div class="font-bold">
+              Ceisa 4.0 </div>
+          </div>
+          <div class="text-[12px]">
+            Application for taxation from customs
+          </div>
+          <div class="border border-black p-1 my-2 rounded-lg w-fit">
+            <div class="font-bold text-[10px]">React</div>
+          </div>
+        </div>
+        <div class="border rounded-lg border-[#3D3C3C] p-5 first:mt-0 my-2 last:mb-0">
+          <div>
+            <div class="font-bold">
+              Alat Monitoring Suhu di Panel Gardu Induk </div>
+          </div>
+          <div class="text-[12px]">
+            Temperature monitoring device for wonogiri substation panel.
+          </div>
+          <div class="border border-black p-1 my-2 rounded-lg w-fit">
+            <div class="font-bold text-[10px]">Arduino</div>
+          </div>
+        </div>
       </div>
 
     </div>
   </div>
+  <button @click="generatePDF">Download PDF</button>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
   layout: 'without-layout',
+})
+
+useHead({
+  title: 'Curriculum Vitae-Muh Syahendra A',
+})
+
+async function generatePDF() {
+
+}
+
+const logos = ref()
+
+onMounted(async () => {
+  logos.value = await Promise.all([
+    import('@/assets/logo/Brand=bootstrap4.jpg'),
+    import('@/assets/logo/Brand=figma.jpg'),
+    import('@/assets/logo/Brand=firebase.jpg'),
+    import('@/assets/logo/Brand=git.jpg'),
+    import('@/assets/logo/Brand=github.jpg'),
+    import('@/assets/logo/Brand=gitlab.jpg'),
+    import('@/assets/logo/Brand=go.jpg'),
+    import('@/assets/logo/Brand=html5.jpg'),
+    import('@/assets/logo/Brand=jira.jpg'),
+    import('@/assets/logo/Brand=mysql.jpg'),
+    import('@/assets/logo/Brand=nextjs.jpg'),
+    import('@/assets/logo/Brand=nodejs.jpg'),
+    import('@/assets/logo/Brand=postgresql.jpg'),
+    import('@/assets/logo/Brand=postman.jpg'),
+    import('@/assets/logo/Brand=reactjs.jpg'),
+    import('@/assets/logo/Brand=tailwind.jpg'),
+    import('@/assets/logo/Brand=typescript.jpg'),
+    import('@/assets/logo/Brand=vitejs.jpg'),
+    import('@/assets/logo/Brand=vuejs.jpg'),
+  ].map(async (module) => ({ src: (await module).default })))
 })
 </script>
