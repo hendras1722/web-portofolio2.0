42b59f80fb93ac4bd48fd393d78895ef93c58153
Muh Syahendra Anindyantoro <57746184+hendras1722@users.noreply.github.com>
2025-01-18 10:01:05 +0700

    chore: update package list boilerplate next

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,13 +2,15 @@
   <section id="hero">
     <div class="fixed top-0">
       <div
-        class="thumbnail relative bg-[url(/coffee.svg)] w-dvw bg-no-repeat bg-center lg:h-[801px] h-[450px] bg-cover bg-scroll">
+        class="thumbnail relative bg-[url(/coffee.svg)] w-dvw bg-no-repeat bg-center lg:h-[801px] h-[450px] bg-cover bg-scroll"
+      >
         <div
           class="lg:px-[157px] sm:px-[100px] px-[20px] w-fit h-fit absolute top-12 bottom-0 mx-auto z-[1] right-0 left-0 my-auto text-white"
           style="
             mix-blend-mode: overlay;
             filter: drop-shadow(0px 5px 10px black);
-          ">
+          "
+        >
           <div class="text-white block">
             <p class="text-center font-bold lg:text-[66px] sm:text-[40px]">
               Reading is essential for those who seek to rise above the ordinary
@@ -26,17 +28,27 @@
       </div>
     </div>
   </section>
-  <div class="lg:px-[160px] lg:py-[99px] px-[50px] py-[20px] z-[5] relative  mt-[400px] dark:bg-[#121212] bg-white"
-    :class="['lg:mt-[800px] md:mt-[394px]'
-    ]">
+  <div
+    class="lg:px-[160px] lg:py-[99px] px-[50px] py-[20px] z-[5] relative mt-[400px] dark:bg-[#121212] bg-white"
+    :class="['lg:mt-[800px] md:mt-[394px]']"
+  >
     <section id="article" class="relative dark:bg-[#121212 ]">
       <div id="article-content" class="flex justify-between">
         <div>
-          <h2 class="font-semibold lg:text-[61px] text-[30px] dark:text-white">Article </h2>
+          <h2 class="font-semibold lg:text-[61px] text-[30px] dark:text-white">
+            Article
+          </h2>
 
           <ContentList path="/blog" v-slot="{ list }">
-            <p v-for="article in filterList(list)" :key="article._path" class="lg:text-[31px] text-[15px]">
-              <NuxtLink :to="article._path" class="hover:underline dark:text-white">
+            <p
+              v-for="article in filterList(list)"
+              :key="article._path"
+              class="lg:text-[31px] text-[15px]"
+            >
+              <NuxtLink
+                :to="article._path"
+                class="hover:underline dark:text-white"
+              >
                 {{ article.title }}
               </NuxtLink>
               <span class="text-[#B1AFB0] ml-2 hover:no-underline">{{
@@ -57,10 +69,18 @@
           <img src="/laptop.png" alt="laptop" />
         </div>
         <div>
-          <h2 class="font-semibold lg:text-[61px] text-[30px] dark:text-white">My Package</h2>
+          <h2 class="font-semibold lg:text-[61px] text-[30px] dark:text-white">
+            My Package
+          </h2>
           <p class="lg:text-[31px] text-[15px]">
-            <NuxtLink :to="item.link" target="_blank" class="hover:underline block w-fit dark:text-white"
-              v-for="(item, index) in packageList" :key="index">{{ item.name }}</NuxtLink>
+            <NuxtLink
+              :to="item.link"
+              target="_blank"
+              class="hover:underline block w-fit dark:text-white"
+              v-for="(item, index) in packageList"
+              :key="index"
+              >{{ item.name }}</NuxtLink
+            >
           </p>
         </div>
       </div>
@@ -100,6 +120,10 @@ useHead({
 })
 
 const packageList = [
+  {
+    name: 'MSA boilerplate next',
+    link: 'https://www.npmjs.com/package/msa-boilerplate-next',
+  },
   {
     name: 'Vue3 Camera Jebreet',
     link: 'https://www.npmjs.com/package/vue3-camera-jebreet',
