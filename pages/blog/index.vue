e86351d52aebfdd4b1a6029c5af3733fcc36a938
hendras1722 <muhsyahendraa1722@gmail.com>
2026-01-17 19:09:29 +0700

    feat: Add BackgroundWaterfall component and integrate it into the blog page with a disabled layout.

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -247,9 +247,22 @@ onMounted(() => {
 </style> -->
 
 
+<script lang="tsx" setup>
+import BackgroundWaterfall from '~/components/BackgroundWaterfall.vue';
+
+definePageMeta({
+  layout: false,
+})
+
+</script>
+
+
 <template>
-  <div class="flex-1 custom-family text-xl px-[50px] lg:px-[250px] py-[20px]">
-    <h1 class="text-5xl text-center">You can read my article here</h1>
-    <div class="text-center text-5xl break-words">Blog is still under maintenance !</div>
+  <div class="relative bg-black min-h-screen">
+    <!-- <BackgroundWaterfall /> -->
+    <div class="flex-1 custom-family text-xl px-[50px] lg:px-[250px] py-[20px]">
+      <h1 class="text-5xl text-center">You can read my article here</h1>
+      <div class="text-center text-5xl break-words">Blog is still under maintenance !</div>
+    </div>
   </div>
 </template>
